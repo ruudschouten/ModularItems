ec9712b234a069cbb073c6b173e18369f679b2de
ruudschouten <ruud.schouten@outlook.com>
2019-11-09 15:33:13 +0100

    Moved some of the options Modifier had to ModifierGroup

--- Assets/Scripts/ScriptableObjects/Types/ModifierGroup.cs
+++ Assets/Scripts/ScriptableObjects/Types/ModifierGroup.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using Stats.Modifiers;
 using UnityEngine;
 
 namespace ScriptableObjects.Types
@@ -6,8 +8,43 @@ namespace ScriptableObjects.Types
     [CreateAssetMenu(fileName = "Modifier Group", menuName = "Modular Items/Modifier Group")]
     public class ModifierGroup : ScriptableObject
     {
-        [SerializeField] private List<Modifier> modifiers;
-        
-        public List<Modifier> Modifiers => modifiers;
+        [SerializeField] private ModifierType type;
+        [SerializeField] private ItemType domain;
+        [SerializeField] private ModifierTier tieredModifiers;
+
+        public ModifierType Type => type;
+        public ModifierTier TieredModifiers => tieredModifiers;
+        public ItemType Domain => domain;
+
+        public ICollection<Modifier> Modifiers
+        {
+            get
+            {
+                if (TieredModifiers == null || TieredModifiers.Values.Count == 0)
+                {
+                    return new List<Modifier>();
+                } 
+                
+                return TieredModifiers.Values;
+            }
+        }
+
+        public Modifier GetByTier(int tier)
+        {
+            if (!tieredModifiers.ContainsKey(tier)) return null;
+            
+            return tieredModifiers[tier];
+        }
+
+        public List<Modifier> GetWithinItemLevel(int level)
+        {
+            if (Modifiers == null || Modifiers.Count == 0)
+            {
+                return new List<Modifier>();
+            }
+            
+            var modifiers = Modifiers.ToList().FindAll(x => x.RequiredItemLevel <= level);
+            return modifiers;
+        }
     }
 }
