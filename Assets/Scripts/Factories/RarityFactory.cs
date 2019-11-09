ab0d99522d73bdee7b5972eefff222ee9250ef9f
ruudschouten <ruud.schouten@outlook.com>
2019-11-09 15:31:08 +0100

    Added RarityFactory which currently returns the index of the ItemRarity
    Still needs some work

---
+++ Assets/Scripts/Factories/RarityFactory.cs
@@ -0,0 +1,33 @@
+﻿using DataStructures.RandomSelector;
+using ScriptableObjects.Types;
+using UnityEngine;
+
+namespace Factories
+{
+    public class RarityFactory : AbstractFactory<ItemRarity>
+    {
+        [SerializeField] private ItemRarityWeights itemRarity;
+
+        private DynamicRandomSelector<int> _selector;
+
+        private void Awake()
+        {
+            _selector = new DynamicRandomSelector<int>();
+
+            var index = 0;
+            foreach (var pair in itemRarity)
+            {
+                _selector.Add(index, pair.Value);
+                
+                index++;
+            }
+
+            _selector.Build((int) seed);
+        }
+        
+        public int GetIndex()
+        {
+            return _selector.SelectRandomItem();
+        }
+    }
+}
